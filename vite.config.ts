040bb4f50e0997681afaf8ce2bc4652ff9c5c304
Alkaidcc <alkaidcc@qq.com>
2023-07-03 20:13:38 +0800

    chore: add config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import UnoCSS from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: [
+        'vue',
+        '@vueuse/core',
+      ],
+      dts: 'src/auto-imports.d.ts',
+      dirs: ['src'],
+      vueTemplate: true,
+    }),
+    Components({
+      resolvers: [AntDesignVueResolver()],
+    }),
+    UnoCSS(),
+    VueDevTools(),
+  ],
 })
