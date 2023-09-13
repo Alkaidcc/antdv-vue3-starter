46e02752140e54bebc935bcd7afefcc1ee1dee1b
Alkaidcc <alkaidcc@qq.com>
2023-09-13 13:45:04 +0800

    chore: init config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
+import { URL, fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import UnoCSS from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
@@ -8,6 +8,11 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   plugins: [
     vue(),
     AutoImport({
@@ -20,9 +25,10 @@ export default defineConfig({
       vueTemplate: true,
     }),
     Components({
-      resolvers: [AntDesignVueResolver()],
+      resolvers: [AntDesignVueResolver({
+        importStyle: false,
+      })],
     }),
-    UnoCSS(),
     VueDevTools(),
   ],
 })
